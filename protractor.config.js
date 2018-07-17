40496dc90239e444f89f0d9a486bea4945202ca1
rgarcia11 <r.garcia11@uniandes.edu.co>
2018-07-17 08:39:38 -0500

    Update protractor.config.js

--- protractor.config.js
+++ protractor.config.js
@@ -1,12 +1,8 @@
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-<<<<<<< HEAD
     specs: ['test/Google.spec.js'],
     onPrepare: () => {
         browser.ignoreSynchronization = true;
     }
-=======
-    specs: ['test/spec.js']
->>>>>>> master
 }
