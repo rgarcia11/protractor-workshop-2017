bcbbc1aca58bc7bcb0ebfc01de6ea22d71801476
Rogelio <r.garcia11@uniandes.edu.co>
2018-07-18 15:46:47 -0500

    Added chrome capabilities

--- protractor/config.ts
+++ protractor/config.ts
@@ -10,6 +10,13 @@ export const config: Config = {
     defaultTimeoutInterval: 120000
   },
   noGlobals: true,
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['disable-infobars=true --window-size=800,600'],
+      prefs: { credentials_enable_service: false }
+    }
+  },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
