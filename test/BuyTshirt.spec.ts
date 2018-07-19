0ae6a7b29362a3eb7c8024c0f18634621a12ad91
rgarcia11 <r.garcia11@uniandes.edu.co>
2018-07-19 11:07:07 -0500

    Several describes (#13)
    * page load and jasmine time reformat
    
    * added implicit wait, deleted all sleep
    
    * explicit waits
    
    * Separated the test into various describe functions

--- test/BuyTshirt.spec.ts
+++ test/BuyTshirt.spec.ts
@@ -10,36 +10,59 @@ import { PaymentStepPage } from '../src/page/PaymentStep.page';
 import { BankPaymentPage } from '../src/page/BankPayment.page';
 import { OrderResumePage } from '../src/page/OrderResume.page';
 
-describe('Buy a t-shirt', () => {
-  const menuContentPage: MenuContentPage = new MenuContentPage();
-  const productDetailPage: ProductDetailPage = new ProductDetailPage();
-  const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
-  const summaryStepPage: SummaryStepPage = new SummaryStepPage();
-  const signInStepPage: SignInStepPage = new SignInStepPage();
-  const addressStepPage: AddressStepPage = new AddressStepPage();
-  const shippingStepPage: ShippingStepPage = new ShippingStepPage();
-  const paymentStepPage: PaymentStepPage = new PaymentStepPage();
-  const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
-  const orderResume: OrderResumePage = new OrderResumePage();
-
-  it('then should be bought a t-shirt', async () => {
+describe('Open a browser page', () => {
+  it('should open', async () => {
     await browser.get('http://automationpractice.com/');
+  });
 
-    await menuContentPage.goToTShirtMenu();
-    await(browser.sleep(3000));
-    await productDetailPage.goToTShirtDetailMenu();
-    await(browser.sleep(3000));
-    await productAddedModalPage.goToAddedItemMenu();
-    await(browser.sleep(3000));
-    await summaryStepPage.goToSummaryMenu();
-    await signInStepPage.goToSignInMenu();
-    await signInStepPage.fillSignInMenu();
-    await addressStepPage.goToAddressMenu();
-    await shippingStepPage.goToShippingMenu();
-    await paymentStepPage.goToPaymentMenu();
-    await bankPaymentPage.goToBankPaymentMenu();
-    await orderResume.goToOrderResume();
-    await expect(orderResume.getOrderResumeStatus())
-          .toBe('Your order on My Store is complete.');
+  describe('Buy a t-shirt', () => {
+    const menuContentPage: MenuContentPage = new MenuContentPage();
+    const productDetailPage: ProductDetailPage = new ProductDetailPage();
+    const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
+    const summaryStepPage: SummaryStepPage = new SummaryStepPage();
+  
+    it('should select a tshit to buy.', async () => {
+      await menuContentPage.goToTShirtMenu();
+      await(browser.sleep(3000));
+      await productDetailPage.goToTShirtDetailMenu();
+      await(browser.sleep(3000));
+      await productAddedModalPage.goToAddedItemMenu();
+      await(browser.sleep(3000));
+      await summaryStepPage.goToSummaryMenu();
+    });
+
+    describe('Sign in', () => {
+      const signInStepPage: SignInStepPage = new SignInStepPage();
+    
+      it('should go to menu, fill contents with test data.', async () => {
+        await signInStepPage.goToSignInMenu();
+        await signInStepPage.fillSignInMenu();
+      });
+
+      describe('Address menu', () => {
+        const addressStepPage: AddressStepPage = new AddressStepPage();
+        const shippingStepPage: ShippingStepPage = new ShippingStepPage();
+      
+        it('should fill address and shipping address with default values.', async () => {
+          await addressStepPage.goToAddressMenu();
+          await shippingStepPage.goToShippingMenu();
+        });
+
+        describe('Payment and summary', () => {
+          const paymentStepPage: PaymentStepPage = new PaymentStepPage();
+          const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
+          const orderResume: OrderResumePage = new OrderResumePage();
+        
+          it('should buy the tshirt and receive successful summary.', async () => {
+            await paymentStepPage.goToPaymentMenu();
+            await bankPaymentPage.goToBankPaymentMenu();
+            await orderResume.goToOrderResume();
+            await expect(orderResume.getOrderResumeStatus())
+                  .toBe('Your order on My Store is complete.');
+          });
+        });
+      });
+    });
   });
 });
+
