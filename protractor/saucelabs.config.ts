252b4f2ae46b027515d74e65515281e0b518f737
rgarcia11 <r.garcia11@uniandes.edu.co>
2018-07-27 15:07:45 -0500

    Saucelabs (#21)
    * Downloads the file correctly, but without extension.
    
    * Static code check
    
    * Added Saucelabs support
    
    * travis support
    
    * Added Travis support fix

---
+++ protractor/saucelabs.config.ts
@@ -0,0 +1,28 @@
+import { browser, Config } from 'protractor';
+import { reporter } from './helpers/reporter';
+
+export const config: Config = {
+  framework: 'jasmine',
+  SELENIUM_PROMISE_MANAGER: false,
+  specs: ['../test/**/*.spec.js'],
+  getPageTimeout: 30000,
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
+  noGlobals: true,
+  capabilities: {
+    name: 'UI Workshop',
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['disable-infobars=true --window-size=800,600'],
+      prefs: { credentials_enable_service: false }
+    }
+  },
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+    browser.manage().timeouts().implicitlyWait(3000);
+    reporter();
+  },
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY
+};
